9b034320deaeded26ab02e9e1e403829252ae975
Asad Rizvi <aley.rizvi@icloud.com>
2025-03-15 02:36:24 +0200

    disable ray-x go plugin and using provided by lazy

--- lua/config/options.lua
+++ lua/config/options.lua
@@ -7,11 +7,11 @@ vim.opt.relativenumber = false
 vim.opt.showtabline = 0
 
 -- Run gofmt + goimports on save
-local format_sync_grp = vim.api.nvim_create_augroup("goimports", {})
-vim.api.nvim_create_autocmd("BufWritePre", {
-  pattern = "*.go",
-  callback = function()
-    require("go.format").goimports()
-  end,
-  group = format_sync_grp,
-})
+-- local format_sync_grp = vim.api.nvim_create_augroup("goimports", {})
+-- vim.api.nvim_create_autocmd("BufWritePre", {
+--   pattern = "*.go",
+--   callback = function()
+--     require("go.format").goimports()
+--   end,
+--   group = format_sync_grp,
+-- })
